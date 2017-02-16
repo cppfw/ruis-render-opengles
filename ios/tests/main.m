45c5349de409a779fb6871dd990bf81627bfcf28
Ivan Gagis <igagis@gmail.com>
2017-02-16 19:21:36 +0200

    stuff

---
+++ ios/tests/main.m
@@ -0,0 +1,11 @@
+//
+//  main.m
+//  tests
+//
+//  Created by Ivan Gagis on 14/02/2017.
+//  Copyright © 2017 Ivan Gagis. All rights reserved.
+//
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
