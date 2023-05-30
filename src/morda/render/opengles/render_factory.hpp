ab861f2d31a1bc808e84021bd7c680b59b7192b2
Ivan Gagis <igagis@gmail.com>
2023-05-30 23:44:12 +0300

    stuff

--- src/morda/render/opengles/render_factory.hpp
+++ src/morda/render/opengles/render_factory.hpp
@@ -34,9 +34,6 @@ public:
 	
 	virtual ~render_factory()noexcept;
 
-	utki::shared_ref<texture_2d> create_texture_2d(rasterimage::image_variant&& imvar)override{
-		throw std::logic_error("unimplemented");
-	}
 	utki::shared_ref<texture_2d> create_texture_2d(const rasterimage::image_variant& imvar)override{
 		throw std::logic_error("unimplemented");
 	}
